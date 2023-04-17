79b5fcba9bbf98ebeb6e28c5e1d202cee5a01775
Eazyisreal <eazyisreal192@gmail.com>
2023-04-17 09:51:43 +0100

    Chore: Take users prompt and output it in the terminal

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
+
+// Import header file
 #include "main.h"
 
 int main (int argc, char **argv){
 	char *prompt = "(shell) $ ";
+	char *lineptr;
+	size_t n = 0;
+
+	(void) argc, (void) argv;
 
 	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s\n", lineptr);
+
+	free(lineptr);
 	return (0);
 }
+
 
