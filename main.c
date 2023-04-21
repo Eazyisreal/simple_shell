718f6fa29b09aaf6520252723fc8b6b00c1adb64
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-04-21 02:25:22 -0700

    Fixing stuffs

--- main.c
+++ main.c
@@ -1,18 +1,79 @@
-// Import header file
 #include "main.h"
 
-int main (int argc, char **argv)
+int token_counter(char *token);
+
+int main(int agrc, char *argv[])
 {
-	char *prompt = "(shell) $ ";
-	char *lineptr;
-	size_t n = 0;
+	char *command = NULL;
+	char *delim = " ";
+	char *token;
+	int i, token_count = 0, word_count = 0;
+	size_t n = 10;
+	pid_t pid;
+	ssize_t num_of_char;
 
-	(void) argc, (void) argv;
+	while (1)
+	{
+		_printf("Shell $ ");
+		num_of_char = getline(&command, &n, stdin);
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+		if (num_of_char == -1)
+		{
+			return (-1);
+		}
 
-	free(lineptr);
+		if (command[num_of_char - 1] == '\n')
+		{
+			command[num_of_char - 1] = '\0';
+			num_of_char--;
+		}
+
+		token = strtok(command, delim);
+
+		i = 1;
+
+		while(token)
+		{
+			token_count = token_counter(token);
+			switch (token_count)
+			{
+				case 'A':
+					break;
+				default:
+					_printf("%s: No such file or directory\n", argv[0]);
+					break;
+			}
+			token = strtok(NULL, delim); 
+		}
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else if (pid == 0)
+		{
+			exit (0);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+
 	return (0);
+}
+
+int token_counter(char *token)
+{
+	int count = 0, i = 0;
+
+	while (token[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
 }
--- main.h
+++ main.h
@@ -3,3 +3,8 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _puts(char *str);
